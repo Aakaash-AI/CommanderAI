ab899dc1b1f6a1d944f3c50a8d4fd49763bab4ad
Aakaash-AI <mookugaming@gmail.com>
2025-12-11 20:04:27 +0530

    Add files via upload

---
+++ electron/main.js
@@ -0,0 +1,25 @@
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+    },
+    autoHideMenuBar: true,
+    backgroundColor: '#0b0f1a'
+  });
+
+  if (process.env.ELECTRON_DEV === 'true') {
+    win.loadURL('http://localhost:5173');
+  } else {
+    win.loadFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit(); });
---
+++ server/config.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  ollamaHost: process.env.OLLAMA_HOST || 'http://localhost:11434',
+  model: process.env.MODEL || 'llama3.2'
+};
